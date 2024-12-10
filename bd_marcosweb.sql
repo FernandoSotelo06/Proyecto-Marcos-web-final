da4607fa39a77c68e0a024208e1c3a722d237719
Fernando Sotelo <137858484+FernandoSotelo06@users.noreply.github.com>
2024-12-10 16:55:46 -0500

    Update bd_marcosweb.sql

--- bd_marcosweb.sql
+++ bd_marcosweb.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 03-11-2024 a las 09:13:57
+-- Servidor: localhost:3306
+-- Tiempo de generación: 10-12-2024 a las 22:54:51
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -29,10 +29,10 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `cliente` (
   `IdCliente` int(11) UNSIGNED NOT NULL,
-  `Dni` varchar(8) DEFAULT NULL,
+  `Dni` varchar(255) NOT NULL,
   `Nombres` varchar(244) DEFAULT NULL,
   `Direccion` varchar(244) DEFAULT NULL,
-  `Estado` varchar(1) DEFAULT NULL
+  `Estado` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -43,7 +43,9 @@ INSERT INTO `cliente` (`IdCliente`, `Dni`, `Nombres`, `Direccion`, `Estado`) VAL
 (17, '2', 'Juan Guerrero Solis', 'Los Alamos', '1'),
 (18, '1', 'Maria Rosas Villanueva', 'Los Laureles 234', '1'),
 (19, '3', 'Andres de Santa Cruz', 'Av. La Frontera 347', '1'),
-(20, '4', 'Andres Mendoza', 'Chosica, Lurigancho', '1');
+(20, '4', 'Andres Mendoza', 'Chosica, Lurigancho', '1'),
+(22, '912731232', 'pepe', 'su casa', 'Activo'),
+(23, '91273112311', 'tito', 'la otra casa', 'Inactivo');
 
 -- --------------------------------------------------------
 
@@ -58,6 +60,23 @@ CREATE TABLE `detalle_ventas` (
   `Cantidad` int(11) UNSIGNED DEFAULT NULL,
   `PrecioVenta` double DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `detalle_ventas`
+--
+
+INSERT INTO `detalle_ventas` (`IdDetalleVentas`, `IdVentas`, `IdProducto`, `Cantidad`, `PrecioVenta`) VALUES
+(155, 94, 1, 1, 150),
+(156, 95, 1, 1, 150),
+(157, 96, 1, 1, 150),
+(158, 96, 2, 3, 20),
+(159, 97, 1, 1, 150),
+(160, 97, 3, 3, 800),
+(161, 98, 1, 1, 150),
+(162, 98, 4, 2, 500),
+(163, 99, 4, 4, 500),
+(164, 99, 19, 4, 50),
+(165, 100, 1, 1, 150);
 
 -- --------------------------------------------------------
 
@@ -84,7 +103,8 @@ INSERT INTO `empleado` (`IdEmpleado`, `Dni`, `Nombres`, `Telefono`, `Estado`, `U
 (3, '123', 'Palermo Suarez', '453536458', '1', 'Em22'),
 (8, '07877541', 'Jesus Lujan', '999666333', '1', 'Lagarto'),
 (9, '07877543', 'Pepito Quispe actu', '999666336', 'Inactivo', ''),
-(11, '07873532', 'Antonio Santiago', '98718240', 'Activo', 'Mr-32');
+(11, '07873532', 'Antonio Santiago', '98718240', 'Activo', 'Mr-32'),
+(19, '72528755', 'Alvaro Chang Espinoza', '999333666', 'Inactivo', 'A-chan1');
 
 -- --------------------------------------------------------
 
@@ -97,7 +117,7 @@ CREATE TABLE `producto` (
   `Nombres` varchar(244) DEFAULT NULL,
   `Precio` double DEFAULT NULL,
   `Stock` int(11) UNSIGNED DEFAULT NULL,
-  `Estado` varchar(1) DEFAULT NULL
+  `Estado` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 --
@@ -105,11 +125,11 @@ CREATE TABLE `producto` (
 --
 
 INSERT INTO `producto` (`IdProducto`, `Nombres`, `Precio`, `Stock`, `Estado`) VALUES
-(1, 'Teclado Logitech 345 Editado', 150, 99, '1'),
-(2, 'Mouse Logitech 567', 20, 98, '1'),
-(3, 'Laptop Lenovo Ideapad 520', 800, 100, '1'),
-(4, 'HeadPhones Sony M333', 500, 98, '1'),
-(7, 'Producto Nuevo w', 22, 35, '1');
+(1, 'Teclado Logitech 345', 150, 94, 'Activo'),
+(2, 'Mouse Logitech 567', 20, 95, 'Activo'),
+(3, 'Laptop Lenovo Ideapad 520', 800, 97, 'Activo'),
+(4, 'HeadPhones Sony M333', 500, 92, 'Activo'),
+(19, 'mouse inalambrico Logitech', 50, 6, 'Activo');
 
 -- --------------------------------------------------------
 
@@ -126,6 +146,19 @@ CREATE TABLE `ventas` (
   `Monto` double DEFAULT NULL,
   `Estado` varchar(1) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Volcado de datos para la tabla `ventas`
+--
+
+INSERT INTO `ventas` (`IdVentas`, `IdCliente`, `IdEmpleado`, `NumeroSerie`, `FechaVentas`, `Monto`, `Estado`) VALUES
+(94, 18, 2, '000000001', '2024-08-12', 150, '1'),
+(95, 18, 2, '000000002', '2024-08-12', 150, '1'),
+(96, 18, 2, '000000003', '2024-08-12', 210, '1'),
+(97, 18, 2, '000000004', '2024-08-12', 2550, '1'),
+(98, 18, 2, '000000005', '2024-08-12', 1150, '1'),
+(99, 18, 2, '000000006', '2024-08-12', 2200, '1'),
+(100, 18, 2, '000000007', '2024-08-12', 150, '1');
 
 --
 -- Índices para tablas volcadas
@@ -173,31 +206,31 @@ ALTER TABLE `ventas`
 -- AUTO_INCREMENT de la tabla `cliente`
 --
 ALTER TABLE `cliente`
-  MODIFY `IdCliente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `IdCliente` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
 
 --
 -- AUTO_INCREMENT de la tabla `detalle_ventas`
 --
 ALTER TABLE `detalle_ventas`
-  MODIFY `IdDetalleVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=155;
+  MODIFY `IdDetalleVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=166;
 
 --
 -- AUTO_INCREMENT de la tabla `empleado`
 --
 ALTER TABLE `empleado`
-  MODIFY `IdEmpleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+  MODIFY `IdEmpleado` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT de la tabla `producto`
 --
 ALTER TABLE `producto`
-  MODIFY `IdProducto` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `IdProducto` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT de la tabla `ventas`
 --
 ALTER TABLE `ventas`
-  MODIFY `IdVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=94;
+  MODIFY `IdVentas` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=101;
 
 --
 -- Restricciones para tablas volcadas
